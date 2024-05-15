e54422c697f4abf27a25608ff3d661de9bf76516
Spikethea <spike00890@hotmail.co.uk>
2024-05-15 12:44:29 +0100

    seats rows

--- src/components/PerspectiveView/Cinema.tsx
+++ src/components/PerspectiveView/Cinema.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense, useEffect } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
+import { Canvas, useLoader, useThree } from '@react-three/fiber';
 import {Instance, Instances, useGLTF} from '@react-three/drei';
 import seatData from 'data/seat-data.json';
 
 import scene from 'assets/models/cinema_seat.glb'
 import placeholder from 'assets/images/placeholder.png'
 import { Material, Mesh } from 'three';
+import { UUID } from 'crypto';
 
 const Seat = (props: any) => {
     const gltf = useGLTF(scene);
@@ -21,17 +22,13 @@ const Seat = (props: any) => {
     )
 }
 
-const SeatsRow = () => {
+const SeatsRow = ({elevation}: {elevation: number, key: number}) => {
     const {nodes, materials} = useGLTF(scene);
 
     const seat = nodes.seat as Mesh;
 
-    useEffect(() => {
-        console.log(nodes)
-    }, [])
-
     return (
-        <Instances range={1000} material={materials.Material} geometry={seat.geometry}>
+        <Instances position-x={-elevation*4} position-y={elevation*3} range={1000} material={materials.Material} geometry={seat.geometry}>
             {seatData.seatRows[0].seats.map((props, i) => (
                 <Seat key={i} {...props} />
             ))}
@@ -39,4 +36,26 @@ const SeatsRow = () => {
     )
 }
 
-export default SeatsRow
+const Cinema = () => {
+    const { camera } = useThree();
+
+    useEffect(()=> {
+        camera.position.x = 15;
+        console.log('update')
+    }, [camera.position])
+
+
+    return (
+        <group position-z={-10}>
+            {
+                seatData.seatRows.map((props, i) => (
+                    <SeatsRow elevation={props.elevation} key={i} />
+                ))
+            }
+            
+        </group>
+        
+    )
+}
+
+export default Cinema
--- src/components/PerspectiveView/PerspectiveView.tsx
+++ src/components/PerspectiveView/PerspectiveView.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense } from 'react';
-import { Canvas, useLoader } from '@react-three/fiber';
+import React, { Suspense, useEffect } from 'react';
+import { Canvas, useLoader, useThree } from '@react-three/fiber';
 import {OrbitControls} from '@react-three/drei';
 import scene from 'assets/models/movietheatre.glb'
 import placeholder from 'assets/images/placeholder.png'
@@ -7,11 +7,11 @@ import SeatsRow from './Cinema';
 
 function Scene(props: any) {
 
+
     return (
             <Canvas>
                 <ambientLight intensity={Math.PI / 2} />
-                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-                <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+                <pointLight position={[10, -10, -10]} decay={0} intensity={Math.PI} />
                 <SeatsRow/>
                 <OrbitControls enablePan={false} enableZoom={false}/>
             </Canvas>
