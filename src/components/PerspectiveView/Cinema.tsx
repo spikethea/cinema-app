5e6b0785e4510610a60183e24c4fbb98361fc36b
Spikethea <spike00890@hotmail.co.uk>
2024-05-15 14:14:48 +0100

    raycast

--- src/components/PerspectiveView/Cinema.tsx
+++ src/components/PerspectiveView/Cinema.tsx
@@ -1,23 +1,57 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useRef } from 'react';
 import { Canvas, useLoader, useThree } from '@react-three/fiber';
 import {Instance, Instances, useGLTF} from '@react-three/drei';
 import seatData from 'data/seat-data.json';
 
 import scene from 'assets/models/cinema_seat.glb'
 import placeholder from 'assets/images/placeholder.png'
-import { Material, Mesh } from 'three';
+import * as THREE from 'three';
 import { UUID } from 'crypto';
 
 const Seat = (props: any) => {
     const gltf = useGLTF(scene);
+    const seatRef = useRef(null);
+    
+
+    const turnSeatRed = () => {
+        if (!seatRef.current) return;
+
+        const seatGroup = seatRef.current as THREE.Group;
+
+        seatGroup.traverse((child)=> {
+            if ( child.name === 'seat' ) {
+                const seat = child as THREE.Mesh;
+                const material = seat.material as THREE.MeshBasicMaterial;
+                console.log(seat)
+                material.color = new THREE.Color(0xFF0000);
+            }
+        })
+    }
+
+    const turnSeatYellow = () => {
+        if (!seatRef.current) return;
+
+        const seatGroup = seatRef.current as THREE.Group;
+
+        seatGroup.traverse((child)=> {
+            if ( child.name === 'seat' ) {
+                const seat = child as THREE.Mesh;
+                const material = seat.material as THREE.MeshBasicMaterial;
+                console.log(seat)
+                material.color = new THREE.Color(0xFFFF00);
+            }
+        })
+    }
 
     return (
                 <Instance
                  castShadow 
                  receiveShadow
                  position={[0, 0, props.seatPos* 3]}
+                 onPointerEnter={turnSeatYellow}
+                 onPointerLeave={turnSeatRed}
                  >
-                    <primitive object={gltf.scene} /> 
+                    <primitive ref={seatRef} object={gltf.scene} /> 
                 </Instance>
     )
 }
@@ -25,7 +59,7 @@ const Seat = (props: any) => {
 const SeatsRow = ({elevation}: {elevation: number, key: number}) => {
     const {nodes, materials} = useGLTF(scene);
 
-    const seat = nodes.seat as Mesh;
+    const seat = nodes.seat as THREE.Mesh;
 
     return (
         <Instances position-x={-elevation*4} position-y={elevation*3} range={1000} material={materials.Material} geometry={seat.geometry}>
